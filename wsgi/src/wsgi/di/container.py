b0d40e5c055f4cb5b678ae259847b09c67b881bc
Diddi Oskarsson <diddi@diddi.se>
2022-11-12 09:47:45 +0100

    Invoke custom methods

--- wsgi/src/wsgi/di/container.py
+++ wsgi/src/wsgi/di/container.py
@@ -1,10 +1,12 @@
 from inspect import signature
-from typing import TypeVar, Dict, Type, cast, Any
+from typing import TypeVar, Dict, Type, cast, Any, Callable, ParamSpec, List
 
 from wsgi.di.service_provider import ServiceProvider
 from wsgi.di.using import Using
 
 T = TypeVar("T")
+P = ParamSpec("P")
+R = TypeVar("R")
 
 
 class Container:
@@ -21,17 +23,34 @@ class Container:
     def add_service(self, typ: Type[T]) -> Using[T]:
         return Using[T](typ, self._provider_callback)
 
-    def resolve(self, typ: Type[T]) -> T:
+    def get_instance(self, typ: Type[T]) -> T:
         provider = self._providers[typ]
         args = []
         kwargs = {}
         for param in provider.get_parameters():
             if param.annotation in self._providers:
                 if param.kind in (param.POSITIONAL_OR_KEYWORD, param.POSITIONAL_ONLY):
-                    args.append(self.resolve(param.annotation))
+                    args.append(self.get_instance(param.annotation))
                 else:
-                    kwargs[param.name] = self.resolve(param.annotation)
+                    kwargs[param.name] = self.get_instance(param.annotation)
 
         for k, v in provider.custom_kwargs.items():
             kwargs[k] = v
         return cast(T, provider.instantiate(args, kwargs))
+
+    def invoke(self, fn: Callable[P, R]) -> R:
+        # We make a slight distinction between getting an instance (effectively invoking the constructor) and invoking
+        # arbitrary callables. This is to support instantiation of singletons, which are handled by the
+        # SingletonProvider.
+        sig = signature(fn)
+        args = []
+        kwargs = {}
+        for param in sig.parameters.values():
+            if param.annotation in self._providers:
+                instance = self.get_instance(param.annotation)
+                if param.kind in (param.POSITIONAL_OR_KEYWORD, param.POSITIONAL_ONLY):
+                    args.append(instance)
+                else:
+                    kwargs[param.name] = instance
+
+        return fn(*args, **kwargs)
--- wsgi/tests/unit/wsgi/di/test_container.py
+++ wsgi/tests/unit/wsgi/di/test_container.py
@@ -16,7 +16,7 @@ class TestContainer(TestCase):
         container.add_service(HttpClient).using(HttpClient)
         container.add_service(Service).using(Service)
 
-        instance = container.resolve(Service)
+        instance = container.get_instance(Service)
         self.assertIsInstance(instance, Service)
         self.assertIsInstance(instance.http, HttpClient)
 
@@ -29,7 +29,7 @@ class TestContainer(TestCase):
         container = Container()
         container.add_service(Service).using(Service, {"name": expected_name})
 
-        instance = container.resolve(Service)
+        instance = container.get_instance(Service)
         self.assertIsInstance(instance, Service)
         self.assertEqual(expected_name, instance.name)
 
@@ -39,6 +39,20 @@ class TestContainer(TestCase):
         container = Container()
         container.add_service(MySingleton).using_singleton(MySingleton)
 
-        instance1 = container.resolve(MySingleton)
-        instance2 = container.resolve(MySingleton)
+        instance1 = container.get_instance(MySingleton)
+        instance2 = container.get_instance(MySingleton)
         self.assertEqual(instance1, instance2)
+
+    def test_method_arguments_can_be_resolved(self) -> None:
+        class Dependency: pass
+        class Service:
+            def do_work(self, dep: Dependency) -> Dependency:
+                return dep
+
+        container = Container()
+        container.add_service(Dependency).using(Dependency)
+        container.add_service(Service).using(Service)
+        instance = container.get_instance(Service)
+
+        dependency = container.invoke(instance.do_work)
+        self.assertIsInstance(dependency, Dependency)
